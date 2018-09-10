1b761c516b8d15bf57aca811ef9333f28a08b340
Nikhil Mittal <nikhil.mittal@tothenew.com>
2018-09-11 02:29:13 +0530

    Overriding with throws

--- src/PolyMorphismExample.java
+++ src/PolyMorphismExample.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
+
 public class PolyMorphismExample {
     public static void main(String[] args) {
         A a = new A();
         System.out.println(a.add(1,2));
 
         a = new B();
+
         System.out.println(a.add(1,2));
 
         System.out.println(a.add(1f,2f));
@@ -12,6 +15,11 @@ public class PolyMorphismExample {
 }
 
 class A {
+
+    public void method1() throws RuntimeException {
+
+    }
+
     public int add(int a, int b) {
         return a+b;
     }
@@ -33,5 +41,9 @@ class B extends A {
     public int add(int a, int b) {
         System.out.println("Child Class Method");
         return super.add(a,b);
+    }
+
+    public void method1() throws IllegalArgumentException {
+
     }
 }
