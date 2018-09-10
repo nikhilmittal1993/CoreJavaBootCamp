a16b4b920a99f0130b1bd84c8c696c22b64ee64e
Nikhil Mittal <nikhil.mittal@tothenew.com>
2018-09-11 01:26:50 +0530

    Added overloading and overriding example

---
+++ src/PolyMorphismExample.java
@@ -0,0 +1,37 @@
+public class PolyMorphismExample {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.add(1,2));
+
+        a = new B();
+        System.out.println(a.add(1,2));
+
+        System.out.println(a.add(1f,2f));
+
+    }
+}
+
+class A {
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a+b+c;
+    }
+
+    public float add(float a, float b) {
+        return a+b;
+    }
+
+    public float add(float a, float b, float c) {
+        return a+b+c;
+    }
+}
+
+class B extends A {
+    public int add(int a, int b) {
+        System.out.println("Child Class Method");
+        return super.add(a,b);
+    }
+}
